45045fa02c60de3e87971630c7b3ae88a3633a35
yumizsui <yumizsui@gmail.com>
2022-07-03 03:18:02 +0900

    places

--- app/src/main/java/jp/ac/titech/itpro/sdl/furui/tsuuch/MainActivity.kt
+++ app/src/main/java/jp/ac/titech/itpro/sdl/furui/tsuuch/MainActivity.kt
@@ -5,25 +5,38 @@ import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Looper
+import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresPermission
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.ktx.api.net.awaitFindCurrentPlace
+import com.google.codelabs.maps.placesdemo.StringUtil
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var infoView: TextView
+    private lateinit var responseView: TextView
     private lateinit var map: GoogleMap
     private lateinit var locationClient: FusedLocationProviderClient
+    private lateinit var placesClient: PlacesClient
     private lateinit var request: LocationRequest
     private lateinit var callback: LocationCallback
     private var ll: LatLng? = null
@@ -31,7 +44,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate")
         setContentView(R.layout.activity_main)
+        Places.initialize(this, BuildConfig.GOOGLE_MAPS_API_KEY)
+        placesClient = Places.createClient(this)
+
         infoView = findViewById(R.id.info_view)
+        responseView = findViewById(R.id.current_response_content)
+
         val fragment =
             supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment?
         if (fragment != null) {
@@ -42,23 +60,27 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         request = LocationRequest.create()
         request.interval = 10000L
         request.fastestInterval = 5000L
-        request.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        request.priority = Priority.PRIORITY_HIGH_ACCURACY
         callback = object : LocationCallback() {
             override fun onLocationResult(locationResult: LocationResult) {
                 Log.d(TAG, "onLocationResult")
                 val location = locationResult.lastLocation
-                ll = LatLng(location.latitude, location.longitude)
+                ll = LatLng(location!!.latitude, location!!.longitude)
                 infoView.text = getString(R.string.latlng_format, ll!!.latitude, ll!!.longitude)
 
             }
         }
         val locateButton = findViewById<Button>(R.id.locate_button)
+        val placeButton = findViewById<Button>(R.id.place_button)
         locateButton.setOnClickListener {
             map.animateCamera(
                 CameraUpdateFactory.newLatLng(
                     ll!!
                 )
             )
+        }
+        placeButton.setOnClickListener {
+            checkPermissionThenFindCurrentPlace()
         }
     }
 
@@ -90,9 +112,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         this.map = map
     }
 
-    @SuppressLint("MissingPermission")
-    private fun startLocationUpdate(reqPermission: Boolean) {
-        Log.d(TAG, "startLocationUpdate")
+    private fun checkPermission(reqPermission: Boolean):Boolean {
         for (permission in PERMISSIONS) {
             if (ContextCompat.checkSelfPermission(
                     this,
@@ -105,10 +125,23 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                     val text = getString(R.string.toast_requires_permission_format, permission)
                     Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
                 }
-                return
+                return false
             }
         }
-        locationClient.requestLocationUpdates(request, callback, Looper.getMainLooper())
+        return true
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdate(reqPermission: Boolean) {
+        Log.d(TAG, "startLocationUpdate")
+        if (checkPermission(reqPermission))
+            locationClient.requestLocationUpdates(request, callback, Looper.getMainLooper())
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun checkPermissionThenFindCurrentPlace() {
+        if (checkPermission(true))
+            findCurrentPlace()
     }
 
     override fun onRequestPermissionsResult(
@@ -137,4 +170,33 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         )
         private const val REQ_PERMISSIONS = 1234
     }
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_WIFI_STATE])
+    private fun findCurrentPlace() {
+        val placeFields: List<Place.Field> =
+            listOf(Place.Field.NAME, Place.Field.ID, Place.Field.ADDRESS, Place.Field.LAT_LNG)
+        val request: FindCurrentPlaceRequest = FindCurrentPlaceRequest.newInstance(placeFields)
+        if (checkPermission(true)) {
+            lifecycleScope.launch {
+                try {
+                    val response = placesClient.awaitFindCurrentPlace(placeFields)
+                    responseView.text = response.prettyPrint()
+                    Log.d(TAG, "okdoke")
+                    Log.d(TAG, response.prettyPrint())
+                    // Enable scrolling on the long list of likely places
+                    val movementMethod = ScrollingMovementMethod()
+                    responseView.movementMethod = movementMethod
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    responseView.text = e.message
+                    Log.d(TAG, "damedoke")
+                    Log.d(TAG, e.message!!)
+                }
+            }
+        }
+    }
+
+}
+fun FindCurrentPlaceResponse.prettyPrint(): String {
+    return StringUtil.stringify(this, false)
 }
