0b126e8dbcec22f66d7995a836d168c51b842b23
yumizsui <yumizsui@gmail.com>
2022-07-04 00:12:44 +0900

    update

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/furui/tsuuch/LocationService.kt
@@ -0,0 +1,214 @@
+package jp.ac.titech.itpro.sdl.furui.tsuuch
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import androidx.annotation.RequiresPermission
+import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.*
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.ktx.api.net.awaitFindCurrentPlace
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class LocationService : Service() {
+    companion object {
+        private val TAG: String = this::class.java.simpleName
+        private const val notificationID = 1
+        private const val notificationIDService = 2
+        const val channelID = "channel2"
+    }
+
+    private lateinit var request: LocationRequest
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var callback: LocationCallback
+    private lateinit var placesClient: PlacesClient
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var notificationManagerService: NotificationManager
+
+    private var previouslyNotifiedStation: Place? = null
+    private var lastStation: Place? = null
+    private var notifyFlag = false
+
+    override fun onCreate() {
+        super.onCreate()
+        placesClient = Places.createClient(this)
+
+        request = LocationRequest.create().apply {
+            interval = 10000L
+            fastestInterval = 5000L
+            priority = Priority.PRIORITY_HIGH_ACCURACY
+        }
+        createNotificationChannel()
+        createNotificationChannelLocationService()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        callback = object : LocationCallback() {
+            @SuppressLint("MissingPermission")
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult.lastLocation?.let{
+                    Log.d(TAG, "onLocationResult: ${it.latitude} , ${it.longitude}")
+                }
+                findCurrentPlace()
+                stationNotification()
+            }
+        }
+
+        val openIntent = Intent(this, MainActivity::class.java).let {
+            PendingIntent.getActivity(this, 0, it, PendingIntent.FLAG_IMMUTABLE)
+        }
+        val notification = NotificationCompat.Builder(this, channelID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("位置情報")
+            .setContentText("\uD83D\uDDFE位置情報を取得中")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(openIntent)
+            .build()
+
+        startForeground(notificationIDService, notification)
+
+        startLocationUpdate()
+
+        return START_STICKY
+    }
+
+
+    override fun onBind(p0: Intent?): IBinder? {
+        Log.d(TAG, "onBind")
+        return null
+    }
+
+    override fun stopService(name: Intent?): Boolean {
+        Log.d(TAG, "stopService")
+        stopLocationUpdate()
+        return super.stopService(name)
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        super.onDestroy()
+        stopLocationUpdate()
+        stopSelf()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdate() {
+        Log.d(TAG, "startLocationUpdate")
+        fusedLocationClient.requestLocationUpdates(request, callback,null)
+    }
+
+    private fun stopLocationUpdate() {
+        Log.d(TAG, "stopLocationUpdate")
+        fusedLocationClient.removeLocationUpdates(callback)
+    }
+
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_WIFI_STATE])
+    fun findCurrentPlace() {
+
+        val placeFields: List<Place.Field> =
+            listOf(Place.Field.NAME, Place.Field.ID, Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.TYPES)
+        CoroutineScope(Dispatchers.Default).launch {
+            try {
+                val response = placesClient.awaitFindCurrentPlace(placeFields)
+                val station = response.findStation()
+
+                notifyFlag = previouslyNotifiedStation?.id != station?.id
+                lastStation = station
+                if (station != null) {
+                    Log.d(TAG, stringify(station))
+                } else {
+                    Log.d(TAG, "No station")
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Log.d(TAG, e.message!!)
+            }
+        }
+    }
+
+    private fun evalFlag():Boolean {
+        val tmp = notifyFlag
+        notifyFlag = false
+        return tmp
+    }
+
+
+
+    private fun createNotificationChannel() {
+        val name = "Tsuuch"
+        val desc = "現在いる駅を通知する"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(MainActivity.channelID, name, importance)
+        channel.description = desc
+        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun createNotificationChannelLocationService() {
+        val name = "Tsuuch Service"
+        val desc = "Foreground Service"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(channelID, name, importance)
+        channel.description = desc
+        notificationManagerService = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManagerService.createNotificationChannel(channel)
+    }
+
+    fun stationNotification(){
+        Log.d(TAG, "stationNotification: $notifyFlag")
+        if (evalFlag()) {
+            previouslyNotifiedStation = lastStation
+
+            val openIntent = Intent(this, MainActivity::class.java).let {
+                PendingIntent.getActivity(this, 0, it, PendingIntent.FLAG_IMMUTABLE)
+            }
+            val notification = NotificationCompat.Builder(this, MainActivity.channelID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("電車は\uD83D\uDE8B 駅\uD83D\uDE89の近くです")
+                .setContentText("今いるのは"+ lastStation!!.name+"\uD83D\uDE89です")
+                .setContentIntent(openIntent)
+                .setOnlyAlertOnce(true)
+                .setAutoCancel(true)
+                .setPriority(NotificationManager.IMPORTANCE_DEFAULT)
+                .build()
+            notificationManager.notify(notificationID, notification)
+//            startForeground(notificationID, notification)
+        }
+    }
+
+}
+fun stringify(place: Place): String {
+    return (place.name!!
+            + " ("
+            + place.id!!
+            + ")"
+            + " is located at "
+            + place.latLng!!.latitude
+            + ", "
+            + place.latLng!!.longitude
+            + "\n"
+            + place.types)
+}
+
+fun FindCurrentPlaceResponse.findStation(): Place? {
+    for (placeLikelihood in this.placeLikelihoods) {
+        if(placeLikelihood.place.types!!.contains(Place.Type.TRAIN_STATION)) {
+            return placeLikelihood.place
+        }
+    }
+    return null
+}
+
--- app/src/main/java/jp/ac/titech/itpro/sdl/furui/tsuuch/MainActivity.kt
+++ app/src/main/java/jp/ac/titech/itpro/sdl/furui/tsuuch/MainActivity.kt
@@ -5,7 +5,6 @@ import android.annotation.SuppressLint
 import android.app.*
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.nfc.Tag
 import android.os.Build
 import android.os.Bundle
 import android.os.Looper
@@ -14,12 +13,9 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.annotation.RequiresPermission
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -27,32 +23,34 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.libraries.places.api.Places
-import com.google.android.libraries.places.api.model.Place
-import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
-import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse
-import com.google.android.libraries.places.api.net.PlacesClient
-import com.google.android.libraries.places.ktx.api.net.awaitFindCurrentPlace
-import kotlinx.coroutines.launch
 import java.util.*
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    companion object {
+        private val TAG: String = this::class.java.simpleName
+        private val PERMISSIONS = arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        )
+        const val channelID = "channel1"
+        const val REQ_PERMISSIONS = 1234
+    }
     private lateinit var infoView: TextView
     private lateinit var responseView: TextView
     private lateinit var map: GoogleMap
     private lateinit var locationClient: FusedLocationProviderClient
-    private lateinit var placesClient: PlacesClient
     private lateinit var request: LocationRequest
     private lateinit var callback: LocationCallback
+    private lateinit var openIntent: PendingIntent
     private lateinit var notificationManager: NotificationManager
+    private lateinit var notificationManagerService: NotificationManager
     private var ll: LatLng? = null
-    private lateinit var pendingIntent: PendingIntent
 
-//    private var stationMemory: MutableList<Place> = mutableListOf()
-    private var previouslyNotifiedStation: Place? = null
-    private var lastStation: Place? = null
-    private var notifyFlag = false
-//    private val threshold = 10
+
+    private var isOnService = false
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,8 +58,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         Log.d(TAG, "onCreate")
         setContentView(R.layout.activity_main)
         Places.initialize(this, BuildConfig.GOOGLE_MAPS_API_KEY, Locale.JAPANESE)
-        placesClient = Places.createClient(this)
-
         infoView = findViewById(R.id.info_view)
         responseView = findViewById(R.id.current_response_content)
 
@@ -72,38 +68,42 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             fragment.getMapAsync(this)
         }
         locationClient = LocationServices.getFusedLocationProviderClient(this)
-        request = LocationRequest.create()
-        request.interval = 10000L
-        request.fastestInterval = 5000L
-        request.priority = Priority.PRIORITY_HIGH_ACCURACY
+        request = LocationRequest.create().apply {
+            interval = 10000L
+            fastestInterval = 5000L
+            priority = Priority.PRIORITY_HIGH_ACCURACY
+        }
         callback = object : LocationCallback() {
             override fun onLocationResult(locationResult: LocationResult) {
-                Log.d(TAG, "onLocationResult")
-                val location = locationResult.lastLocation
-                ll = LatLng(location!!.latitude, location!!.longitude)
-                infoView.text = getString(R.string.latlng_format, ll!!.latitude, ll!!.longitude)
-                checkPermissionThenFindCurrentPlace()
-                stationNotification()
+                if (!isOnService) {
+                    Log.d(TAG, "onLocationResult")
+
+                    locationResult.lastLocation?.let{
+                        ll = LatLng(it.latitude, it.longitude)
+                    }
+                    infoView.text = getString(R.string.latlng_format, ll!!.latitude, ll!!.longitude)
+                }
             }
         }
-        val locateButton = findViewById<Button>(R.id.locate_button)
-        val placeButton = findViewById<Button>(R.id.place_button)
-        locateButton.setOnClickListener {
-            map.animateCamera(
-                CameraUpdateFactory.newLatLng(
-                    ll!!
-                )
-            )
+        openIntent = Intent(this, MainActivity::class.java).let {
+            PendingIntent.getActivity(this, 0, it, PendingIntent.FLAG_IMMUTABLE)
         }
-        createNotificationChannel()
-        val intent = Intent(this, MainActivity::class.java)
-        pendingIntent =
-            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
-        placeButton.setOnClickListener {
-            checkPermissionThenFindCurrentPlace()
+        val foreButton = findViewById<Button>(R.id.fore_button)
+        foreButton.setOnClickListener {
+            val intent = Intent(this, LocationService::class.java)
+            if (!isOnService){
+                if(checkPermission(true)){
+                    isOnService=true
+                    startForegroundService(intent)
+                }
+            } else {
+                isOnService=false
+                stopService(intent)
+            }
         }
     }
+
 
     override fun onStart() {
         super.onStart()
@@ -123,8 +123,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onStop() {
-        Log.d(TAG, "onStop")
         super.onStop()
+        Log.d(TAG, "onStop")
     }
 
     override fun onMapReady(map: GoogleMap) {
@@ -133,25 +133,14 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         this.map = map
     }
 
-    private fun checkPermission(reqPermission: Boolean):Boolean {
-        for (permission in PERMISSIONS) {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    permission
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                if (reqPermission) {
-                    ActivityCompat.requestPermissions(this, PERMISSIONS, REQ_PERMISSIONS)
-                } else {
-                    val text = getString(R.string.toast_requires_permission_format, permission)
-                    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
-                }
-                return false
-            }
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        if(isOnService){
+            val intent = Intent(this, LocationService::class.java)
+            stopService(intent)
         }
-        return true
+        super.onDestroy()
     }
-
     @SuppressLint("MissingPermission")
     private fun startLocationUpdate(reqPermission: Boolean) {
         Log.d(TAG, "startLocationUpdate")
@@ -159,11 +148,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             locationClient.requestLocationUpdates(request, callback, Looper.getMainLooper())
     }
 
-    @SuppressLint("MissingPermission")
-    private fun checkPermissionThenFindCurrentPlace() {
-        if (checkPermission(true))
-            findCurrentPlace()
-    }
 
     override fun onRequestPermissionsResult(
         reqCode: Int,
@@ -183,34 +167,46 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         locationClient.removeLocationUpdates(callback)
     }
 
-    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_WIFI_STATE])
-    private fun findCurrentPlace() {
-
-        val placeFields: List<Place.Field> =
-            listOf(Place.Field.NAME, Place.Field.ID, Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.TYPES)
-        val request: FindCurrentPlaceRequest = FindCurrentPlaceRequest.newInstance(placeFields)
-        if (checkPermission(true)) {
-            lifecycleScope.launch {
-                try {
-                    val response = placesClient.awaitFindCurrentPlace(placeFields)
-                    val station = response.findStation()
-
-                    notifyFlag = previouslyNotifiedStation?.id != station?.id
-                    lastStation = station
-                    if (station != null) {
-                        responseView.text = response.prettyPrint()
-                    }
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    responseView.text = e.message
-                    Log.d(TAG, e.message!!)
+    private fun checkPermission(reqPermission: Boolean):Boolean {
+        for (permission in arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION)) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                if (reqPermission) {
+                    ActivityCompat.requestPermissions(this,
+                        PERMISSIONS,
+                        REQ_PERMISSIONS
+                    )
+                } else {
+                    val text = getString(R.string.toast_requires_permission_format, permission)
+                    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
                 }
+                return false
             }
         }
+        if (ActivityCompat
+                .checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED){
+            if(reqPermission){
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                    REQ_PERMISSIONS
+                )
+            }else {
+                val text = getString(R.string.toast_requires_permission_format, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+            }
+            return false
+
+        }
+        return true
     }
 
-    private fun createNotificationChannel()
-    {
+    private fun createNotificationChannel() {
         val name = "Tsuuch"
         val desc = "現在いる駅を通知する"
         val importance = NotificationManager.IMPORTANCE_DEFAULT
@@ -220,80 +216,16 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         notificationManager.createNotificationChannel(channel)
     }
 
-    private fun evalFlag():Boolean {
-        val tmp = notifyFlag
-        notifyFlag = false
-        return tmp
-    }
-    private fun stationNotification(){
-        Log.d(TAG, "stationNotification"+notifyFlag)
-        if (evalFlag()) {
-            previouslyNotifiedStation = lastStation
-            val notification = NotificationCompat.Builder(this, channelID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle("電車は\uD83D\uDE8B 駅\uD83D\uDE89の近くです")
-                .setContentText("今いるのは"+ lastStation!!.name+"\uD83D\uDE89です")
-                .setContentIntent(pendingIntent)
-                .setOnlyAlertOnce(true)
-                .setAutoCancel(true)
-                .setPriority(NotificationManager.IMPORTANCE_DEFAULT)
-                .build()
-            notificationManager.notify(notificationID, notification)
-        }
+    private fun createNotificationChannelLocationService() {
+        val name = "Tsuuch Service"
+        val desc = "Foreground Service"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(LocationService.channelID, name, importance)
+        channel.description = desc
+        notificationManagerService = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManagerService.createNotificationChannel(channel)
     }
 
-    companion object {
-        private val TAG: String = MainActivity::class.java.getSimpleName()
-        private val PERMISSIONS = arrayOf(
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        )
-        private const val REQ_PERMISSIONS = 1234
-        private const val notificationID = 1
-        private const val channelID = "channel1"
-
-    }
-
-}
 
 
-fun stringify(place: Place): String {
-    return (place.name
-            + " ("
-            + place.id
-            + ")"
-            + " is located at "
-            + place.latLng.latitude
-            + ", "
-            + place.latLng.longitude
-            + "\n"
-            + place.types)
-}
-
-fun FindCurrentPlaceResponse.prettyPrint(): String {
-
-    val FIELD_SEPARATOR = "\n\t"
-    val RESULT_SEPARATOR = "\n---\n\t"
-    val response = this
-    val builder = StringBuilder()
-    var train_place: Place? = null
-    builder.append(response.placeLikelihoods.size).append(" Current Place Results:")
-    for (placeLikelihood in response.placeLikelihoods) {
-        if(placeLikelihood.place.types.contains(Place.Type.TRAIN_STATION)) {
-            train_place = placeLikelihood.place
-            builder.append(stringify(placeLikelihood.place))
-            break
-        }
-    }
-    return builder.toString()
-}
-
-
-fun FindCurrentPlaceResponse.findStation(): Place? {
-    for (placeLikelihood in this.placeLikelihoods) {
-        if(placeLikelihood.place.types.contains(Place.Type.TRAIN_STATION)) {
-            return placeLikelihood.place
-        }
-    }
-    return null
 }
